2ed33e6c0902150a187aa91e371ece3b3b4765fd
Wayne Dunkley <dev@waynedunkley.com>
2015-09-11 10:56:43 +1000

    ton of uploads

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,17 @@
+Template.postSubmit.events({
+  'submit form' : function(e){
+    e.preventDefault();
+
+    var post = {
+      title: $(e.target).find('[name=title]').val(),
+      description: $(e.target).find('[name=description]').val()
+    };
+
+    Meteor.call('post', post, function(error, id) {
+      if (error)
+        return alert(error.reason);
+
+      Router.go('postPage', {_id: id});
+    });
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,16 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+
+// Meteor.publish('userData', function () {
+//   if (this.userId) {
+//     return Meteor.users.find({
+//       _id: this.userId
+//     },{
+//       createdAt: 1,
+//       services: false
+//     });
+//   } else {
+//     this.ready();
+//   }
+// });
